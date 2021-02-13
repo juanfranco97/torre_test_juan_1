5ad7d67488a22fa3eeeceac91e81455ef64d2dd9
franco123 <juanfranco1997mi@gmail.com>
2021-02-12 22:04:33 -0600

    add new rute, /predict

--- api/server.py
+++ api/server.py
@@ -1,6 +1,6 @@
 """This is the main  file that control all aplication internal  flow"""
 #Flask
-from flask import Flask, make_response, jsonify, redirect, url_for
+from flask import Flask, jsonify
 from ../configuration import Configuration
 from flask_cors import CORS, cross_origin
 from utils import Utils
@@ -19,11 +19,17 @@ app.config['CORS_HEADERS'] = 'torre_test'
 @app.route('/', methods=['GET'])
 @cross_origin()
 def init():
-    return "Welcome to We deal, this api is only available to We Deal developers"
+    return "Welcome to my test in torre"
 
-
+@app.route('/predict/<id_job_offer>', methods=['GET'])
+@cross_origin()
+def predict(id_job_offer):
+    X_test = Utils.get_x_test(id_job_offer)
+    #prediction = model.predict(X_test.reshape(1,-1))
+    return jsonify({'prediccion' : "hola"})
 
 
 
 if __name__ == "__main__":
+    model = joblib.load('./models/best_model.pkl')
     app.run()
